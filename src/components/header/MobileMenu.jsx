125f58205c138e303be74b17b5ffcfa7fb093de8
reedsans <oladsposanusi@gmail.com>
2025-03-22 23:27:10 +0100

    Improved MobileMenu linking

--- src/components/header/MobileMenu.jsx
+++ src/components/header/MobileMenu.jsx
@@ -1,10 +1,6 @@
 import { Link } from "react-router-dom"
 
 const MobileMenu = ({ isOpen, toggle }) => {
-
-
-  console.log(isOpen)
-
   return (
     <>
     
@@ -13,12 +9,12 @@ const MobileMenu = ({ isOpen, toggle }) => {
       <span className={`bg-white rounded-md w-full h-[2px] transition-all duration-500 ease-in-out ${isOpen ? 'opacity-0' : ''}`}></span>
       <span className={`bg-white rounded-md w-full h-[2px] transition-all duration-500 ease-in-out ${isOpen ? '-rotate-45 translate-x-[3px] translate-y-[-8px]' : ''}`}></span>
     </button>
-    <ul className={`lg:hidden | fixed text-2xl font-slim uppercase bg-black p-7 flex flex-col justify-center  h-screen w-full top-0 transition-all duration-700 ease-in-out ${isOpen ? 'left-0' : 'left-[-1000px]'} z-10`}>
-      <li className='py-4'><Link to='/'>About</Link></li>
-      <li className='py-4'><Link to='/careers'>Careers</Link></li>
-      <li className='py-4'><Link to='/events'>Events</Link></li>
-      <li className='py-4'><Link to='/products'>Products</Link></li>
-      <li className='py-4'><Link to='/support'>Support</Link></li>
+    <ul className={`lg:hidden | fixed text-2xl font-slim uppercase bg-black p-7 flex flex-col justify-center h-screen w-full top-0 transition-all duration-700 ease-in-out ${isOpen ? 'left-0' : 'left-[-1000px]'} z-10`}>
+      <li className='py-4 hover:bg-white hover:text-black  hover:font-semibold px-3 mt-1 transition-colors duration-300 ease-in-out'><Link onClick={toggle} to='/'>About</Link></li>
+      <li className='py-4 hover:bg-white hover:text-black hover:font-semibold px-3 mt-1 transition-colors duration-300 ease-in-out'><Link onClick={toggle} to='/careers'>Careers</Link></li>
+      <li className='py-4 hover:bg-white hover:text-black hover:font-semibold px-3 mt-1 transition-colors duration-300 ease-in-out'><Link onClick={toggle} to='/events'>Events</Link></li>
+      <li className='py-4 hover:bg-white hover:text-black hover:font-semibold px-3 mt-1 transition-colors duration-300 ease-in-out'><Link onClick={toggle} to='/products'>Products</Link></li>
+      <li className='py-4 hover:bg-white hover:text-black hover:font-semibold px-3 mt-1 transition-colors duration-300 ease-in-out'><Link onClick={toggle} to='/support'>Support</Link></li>
     </ul>
     </>
     
--- src/components/mainContent/OurCreations.jsx
+++ src/components/mainContent/OurCreations.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const OurCreations = () => {
   return (
-    <section className="mt-40">
+    <section className="mt-56 lg:mt-40">
       <div className="flex flex-col items-center lg:flex-row justify-between ">
         <h2 className="text-4xl font-slim tracking-wide">OUR CREATIONS</h2>
         <button className="hidden lg:inline-block px-8 py-2 tracking-widest bg-white hover:bg-black border-2 border-black hover:border-white text-black hover:text-white transition-all duration-300 ease-in-out">
           SEE ALL
         </button>
       </div>
-      <div className="flex flex-col lg:flex-row lg:flex-wrap justify-between mt-20 gap-y-6 gap-x-1 mx-auto">
+      <div className="flex flex-col lg:flex-row lg:flex-wrap justify-between mt-12 lg:mt-20 gap-y-6 gap-x-1 mx-auto">
         <div className="relative hover:opacity-40  w-full lg:basis-1/5 lg:w-1/5 hover:text-black text-white">
           <img
             src="../images/desktop/image-night-arcade.jpg"
